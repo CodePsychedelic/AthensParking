d541ae8b5b68ab39c73a94ff0e3ac7d546028826
Alkis <gk.alkiviadis@gmail.com>
2024-01-14 17:05:39 +0200

    Initial commit

---
+++ sprintboot-backend/src/main/java/com/example/AthensParking/AthensParkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.AthensParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthensParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthensParkingApplication.class, args);
+	}
+
+}
---
+++ sprintboot-backend/src/test/java/com/example/AthensParking/AthensParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AthensParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthensParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
