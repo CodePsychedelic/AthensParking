f329692669e6e3d65825627ecd3497b31a1a7d42
man-chro96 <manolis.chro@gmail.com>
2024-01-15 11:56:51 +0200

    Initial setup of fe layer

---
+++ frontend/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+   output: {
+      path: path.join(__dirname, "/dist"), 
+      filename: "bundle.js", 
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: "src/index.html", 
+      }),
+   ],
+   devServer: {
+      port: 3000, 
+      historyApiFallback: {
+         index: 'dist/index.html'
+      }
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/, 
+            exclude: /node_modules/,
+            use: {
+               loader: "babel-loader",
+            },
+         },
+         {
+            test: /\.(sa|sc|c)ss$/, 
+            use: ["style-loader", "css-loader", "sass-loader"],
+         },
+         {
+            test: /\.(png|jpg|woff|woff2|eot|ttf|svg|gif)$/,
+            loader: "url-loader",
+            options: { 
+               limit: false,
+               name: 'assets/[name].[ext]',
+            },
+         },
+      ],
+   },
+   resolve: {
+      extensions: [".js", ".jsx"],
+   },
+};
